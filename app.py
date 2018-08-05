1a9a761c11fc16502cec34f1ac0aff84936ad75b
Behoston <mlegiecki@gmail.com>
2018-08-05 22:47:50 +0200

    Dodane limity i fix importów.

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
 import yaml
 from feedgen.feed import FeedGenerator
 from sanic import Sanic
-from sanic.response import json, HTTPResponse
+from sanic.response import HTTPResponse
+from sanic.response import json
 
 import models
 from bot.message import float_fucker
 
 app = Sanic()
+
+DEFAULT_PROMOTION_LIMIT = 5
+MAX_PROMOTION_LIMIT = 20
 
 
 @app.route('/doc')
@@ -19,12 +23,13 @@ async def doc(request):
 async def last_promotions(request):
     shops = models.Shop.select()
     response = []
+    limit = min(int(request.args.get('limit', DEFAULT_PROMOTION_LIMIT)), MAX_PROMOTION_LIMIT)
     for shop in shops:
         response.append({
             'name': shop.name,
             'last_promotions': [
                 serialize_promotion(promotion)
-                for promotion in shop.last_promotions(6)
+                for promotion in shop.last_promotions(limit)
             ]
         })
     return json(response)
@@ -48,8 +53,9 @@ async def rss(request):
     fg.link(href='https://cebul.ga/rss')
     fg.description("Najnowsze promocje ze sklepów z elektroniką użytkową.")
     shops = models.Shop.select()
+    limit = min(int(request.args.get('limit', DEFAULT_PROMOTION_LIMIT)), MAX_PROMOTION_LIMIT)
     for shop in shops:
-        for promotion in shop.last_promotions(6):
+        for promotion in shop.last_promotions(limit):
             fe = fg.add_entry()
             fe.title(
                 "[{new_price} PLN] {product_name}".format(
